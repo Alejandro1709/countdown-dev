ad19ebfd17c57d85f264bbfecdca7c9205db868c
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 18:00:55 -0500

    Add: client - routing and homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react'
+import {Routes, Route} from 'react-router-dom'
 import Container from './components/Container'
 import Header from './components/Header'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <Fragment>
       <Container>
         <Header />
+        <div className=''>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
       </Container>
     </Fragment>
   )
