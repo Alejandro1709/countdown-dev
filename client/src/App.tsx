49652065bda3e549d54bb201ac9c3d9331ecb77e
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 17:50:05 -0500

    Add: client - navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Fragment>
+      {/* App Container */}
+      <div className='mx-12'>
+        <header className="header flex justify-between items-center p-4 my-6 bg-gray-100">
+          <span>Left</span>
+          <h2 className='text-2xl font-semibold'>Countdowns</h2>
+          <span>Login</span>
+        </header>
+      </div>
+    </Fragment>
   )
 }
 
