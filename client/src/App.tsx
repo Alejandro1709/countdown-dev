028aa6de11e416c13bbb4aed769d20828a67d250
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 18:46:04 -0500

    Add: login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Container from './components/Container'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
           </Routes>
         </main>
       </Container>
