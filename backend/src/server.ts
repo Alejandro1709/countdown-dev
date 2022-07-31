78f8f45c4c0bff551ce253ed752bbdb8f4def283
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 13:11:14 -0500

    Initial Commit, install dependencies, basic server

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello API')
+})
+
+// Error Handling
+
+// Server Listening
+
+const PORT = process.env.PORT || 2023
+
+app.listen(PORT, () => console.log(`Server is up and running on port: ${PORT}`))
