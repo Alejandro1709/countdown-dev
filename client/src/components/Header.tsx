6770d0e6a6a4b9e74f6ffd6cbc6241b3af023da7
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 18:42:52 -0500

    Add: client - main ui

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className="header flex justify-between items-center p-4 my-6 bg-gray-100">
+    <header className="header flex justify-between items-center p-4 my-6 bg-red-400 text-white shadow-md select-none">
       <span>Left</span>
       <h2 className='text-2xl font-semibold'>Countdowns</h2>
       <span>Login</span>
--- client/src/components/Item.tsx
+++ client/src/components/Item.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Item() {
   return (
-    <div className='p-8 bg-gray-200'>Item #1</div>
+    <div className='flex justify-between bg-red-400 cursor-pointer hover:shadow-xl transition-all select-none'>
+      <div className='p-3 ml-2'>
+        <div className="flex items-center gap-4">
+          <h1 className='text-3xl'>🤯</h1>
+          <div className=''>
+            <h2 className='text-1xl font-bold text-white'>Trip to California</h2>
+            <p className='font-semibold text-gray-300'>September 17, 2022</p>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col items-center justify-center bg-red-500 p-2'>
+        <h3 className='text-2xl font-semibold text-white'>10</h3>
+        <p className='text-sm text-white'>days left</p>
+      </div>
+    </div>
   )
 }
 
