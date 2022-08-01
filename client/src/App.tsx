e7a013538efee967bc21c08f8e9c5d9b377faf04
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 19:03:26 -0500

    Add: client - new page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Container from './components/Container'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import NewPage from './pages/NewPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/login' element={<LoginPage />} />
+            <Route path='/countdowns/new' element={<NewPage />} />
           </Routes>
         </main>
       </Container>
