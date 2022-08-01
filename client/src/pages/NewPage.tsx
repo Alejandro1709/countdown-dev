fcabd05542dae3436a284f78367676fe6e693e68
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-07-31 19:26:43 -0500

    Add: client - create countdown ui

--- client/src/pages/NewPage.tsx
+++ client/src/pages/NewPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NewPage() {
   return (
-    <div>NewPage</div>
+    <div className='px-2'>
+      <form className='flex flex-col gap-4'>
+        <div className="flex flex-col">
+          <label htmlFor="title">Title:</label>
+          <input className="p-2 rounded-md border" type="text" id="title" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="date">Date:</label>
+          <input className="p-2 rounded-md border" type="date" id="date" />
+        </div>
+        <button className='text-white bg-blue-400 p-2 rounded-md hover:bg-blue-500' type='submit'>Create</button>
+      </form>
+      <p className='bg-gray-300 p-2 text-center rounded-md hover:bg-gray-400 w-full mt-2 cursor-pointer'>
+        <Link to="/">Cancel</Link>
+      </p>
+    </div>
   )
 }
 
