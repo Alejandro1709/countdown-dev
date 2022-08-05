d283645e9c89bffb0b1b28d1334a630a63246371
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-08-04 20:02:46 -0500

    Add: client - detail page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Container from './components/Container'
 import Header from './components/Header'
+import DetailPage from './pages/DetailPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import NewPage from './pages/NewPage'
@@ -16,6 +17,7 @@ function App() {
             <Route path='/' element={<HomePage />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/countdowns/new' element={<NewPage />} />
+            <Route path='/countdowns/:id' element={<DetailPage />} />
           </Routes>
         </main>
       </Container>
--- client/src/components/Item.tsx
+++ client/src/components/Item.tsx
@@ -3,6 +3,7 @@ import Moment from 'react-moment'
 import Countdown from '../types/countdown'
 import { countDownFromDate } from '../utils/dateUtilities'
 import 'moment-timezone'
+import { Link } from 'react-router-dom'
 
 type ItemProps = {
   item: Countdown
@@ -18,23 +19,25 @@ function Item({ item }: ItemProps) {
   }, [])
   
   return (
-    <div className='flex justify-between bg-red-400 cursor-pointer hover:shadow-xl transition-all select-none'>
-      <div className='p-3 ml-2'>
-        <div className="flex items-center gap-4">
-          <h1 className='text-3xl'>🤯</h1>
-          <div className=''>
-            <h2 className='text-1xl font-bold text-white'>{item.title}</h2>
-            <p className='font-semibold text-gray-300'>
-              <Moment format='LLLL'>{item.toDate}</Moment>
-            </p>
+    <Link to={`/countdowns/${item._id}`}>
+      <div className='flex justify-between bg-red-400 cursor-pointer hover:shadow-xl transition-all select-none'>
+        <div className='p-3 ml-2'>
+          <div className="flex items-center gap-4">
+            <h1 className='text-3xl'>🤯</h1>
+            <div className=''>
+              <h2 className='text-1xl font-bold text-white'>{item.title}</h2>
+              <p className='font-semibold text-gray-300'>
+                <Moment format='LLLL'>{item.toDate}</Moment>
+              </p>
+            </div>
           </div>
         </div>
+        <div className='flex flex-col items-center justify-center bg-red-500 p-2'>
+          <h3 className='text-2xl font-semibold text-white'>{currentDayCount < 0 ? Math.abs(currentDayCount) : currentDayCount}</h3>
+          <p className='text-sm text-white'>{currentDayCount < 0 ? 'days ago' : 'days left'}</p>
+        </div>
       </div>
-      <div className='flex flex-col items-center justify-center bg-red-500 p-2'>
-        <h3 className='text-2xl font-semibold text-white'>{currentDayCount < 0 ? Math.abs(currentDayCount) : currentDayCount}</h3>
-        <p className='text-sm text-white'>{currentDayCount < 0 ? 'days ago' : 'days left'}</p>
-      </div>
-    </div>
+    </Link>
   )
 }
 
