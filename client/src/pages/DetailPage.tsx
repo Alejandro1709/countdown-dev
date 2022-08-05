2b60ac9ee64fa61d402edee253c4c9549b89579b
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-08-04 20:57:07 -0500

    Add client - edit function

--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -1,20 +1,54 @@
-import React, {useState, useEffect, Fragment} from 'react'
-import { useParams } from 'react-router-dom'
+import React, {useState, useEffect, Fragment, FormEvent} from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import Item from '../components/Item'
 import Countdown from '../types/countdown'
 import axios from 'axios'
 
 export default function DetailPage() {
+  const [title, setTitle] = useState<string>('')
+  const [toDate, setToDate] = useState<string>('')
   const [countdown, setCountdown] = useState<Countdown>()
   const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>('')
 
   const {id} = useParams()
+
+  const navigate = useNavigate()
 
   const handleGetCountdown = async (id: string) => {
     try {
       const {data} = await axios.get(`/api/v1/countdowns/${id}`)
       console.log(data)
       setCountdown(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handleUpdateCountdown = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!title || !toDate) {
+      setMessage('Please fill out the fields')
+      return
+    }
+
+    setIsLoading(true)
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+
+      const { data } = await axios.patch(`/api/v1/countdowns/${id}`, { title, toDate }, config)
+      console.log(data)
+
+      setIsLoading(false)
+
+      navigate('/')
     } catch (error) {
       console.error(error)
     }
@@ -29,6 +63,8 @@ export default function DetailPage() {
 
   return (
     <Fragment>
+      {isLoading && <p className='p-4 bg-blue-300 text-white mb-2'>Loading...</p>}
+      {message &&  <p className='p-4 bg-blue-300 text-white mb-2'>{message}</p>}
       {countdown ? <Item item={countdown} isSelectable={false}/> : <p className='p-4 bg-red-300 text-white mb-2'>Countdown not found</p> }
       <div className="editable mt-6">
         <div className="flex flex-row justify-between gap-4">
@@ -37,14 +73,18 @@ export default function DetailPage() {
         </div>
         {isEditing ? (
           <Fragment>
-            <form className='flex flex-col gap-4 mt-6'>
+            <form className='flex flex-col gap-4 mt-6' onSubmit={handleUpdateCountdown}>
               <div className="flex flex-col">
                 <label htmlFor="title">Title:</label>
-                <input className="p-2 rounded-md border" name="title" type="text" id="title" placeholder={countdown?.title} />
+                <input className="p-2 rounded-md border" name="title" type="text" id="title"
+                  placeholder={countdown?.title} value={title}
+                  onChange={(e) => setTitle(e.target.value)} />
               </div>
               <div className="flex flex-col">
                 <label htmlFor="date">Date:</label>
-                <input className="p-2 rounded-md border" name="toDate" type="date" id="date" value={countdown?.toDate} />
+                <input className="p-2 rounded-md border" name="toDate" type="date" id="date"
+                  value={toDate}
+                  onChange={(e) => setToDate(e.target.value)} />
               </div>
               <button className='text-white bg-blue-400 p-2 rounded-md hover:bg-blue-500' type='submit'>Update</button>
             </form>
