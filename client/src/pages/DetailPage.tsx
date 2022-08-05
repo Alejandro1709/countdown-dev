c0c71b97db6b747a7b87e048995ffc169608a08c
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-08-04 20:35:33 -0500

    Add: Countdown page and toggle state for showing edit form

--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
 import { useParams } from 'react-router-dom'
 import Item from '../components/Item'
 import Countdown from '../types/countdown'
@@ -6,6 +6,7 @@ import axios from 'axios'
 
 export default function DetailPage() {
   const [countdown, setCountdown] = useState<Countdown>()
+  const [isEditing, setIsEditing] = useState<boolean>(false)
 
   const {id} = useParams()
 
@@ -27,8 +28,32 @@ export default function DetailPage() {
   
 
   return (
-    <div>
+    <Fragment>
       {countdown ? <Item item={countdown} isSelectable={false}/> : <p className='p-4 bg-red-300 text-white mb-2'>Countdown not found</p> }
-    </div>
+      <div className="editable mt-6">
+        <div className="flex flex-row justify-between gap-4">
+          <button className='p-2 bg-blue-400 text-white w-full rounded-md hover:bg-blue-500' onClick={() => setIsEditing(true)}>Edit</button>
+          <button className='p-2 bg-red-400 text-white w-full rounded-md hover:bg-red-500'>Delete</button>
+        </div>
+        {isEditing ? (
+          <Fragment>
+            <form className='flex flex-col gap-4 mt-6'>
+              <div className="flex flex-col">
+                <label htmlFor="title">Title:</label>
+                <input className="p-2 rounded-md border" name="title" type="text" id="title" placeholder={countdown?.title} />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="date">Date:</label>
+                <input className="p-2 rounded-md border" name="toDate" type="date" id="date" value={countdown?.toDate} />
+              </div>
+              <button className='text-white bg-blue-400 p-2 rounded-md hover:bg-blue-500' type='submit'>Update</button>
+            </form>
+            <button className='bg-gray-300 p-2 text-center rounded-md hover:bg-gray-400 w-full mt-2 cursor-pointer' onClick={() => setIsEditing(false)}>
+            Cancel
+            </button>
+          </Fragment>
+        ) : null}
+      </div>
+    </Fragment>
   )
 }
